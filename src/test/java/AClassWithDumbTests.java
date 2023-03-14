4774fce77e58467f83948a6012550f75aed69a4c
aleksej <aleksej.afonin@gmail.com>
2023-03-14 12:54:46 +0700

    add Disabled test

--- src/test/java/AClassWithDumbTests.java
+++ src/test/java/AClassWithDumbTests.java
@@ -1,7 +1,4 @@
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
@@ -21,45 +18,50 @@ public class AClassWithDumbTests {
     }
 
     @Test
-    @DisplayName("First dumb test")
+    @DisplayName("First dumb test.")
     void demoTestMethodI() {
         assertTrue(i < 1, "This message newer will display.");
     }
 
     @RepeatedTest(3)
-    @DisplayName("Second dumb test")
+    @DisplayName("Second dumb test.")
     void demoTestMethodJ() {
         assertFalse(j++ > 1, "This message will display, when test fail.");
     }
 
     @Test
-    @DisplayName("Unconditional Error")
+    @DisplayName("Unconditional Error.")
     void testUnconditionalError() {
         fail("This test is newer pass.");
     }
 
     @Test
-    @DisplayName("TimeOut test")
+    @DisplayName("TimeOut test.")
     void testTimeOutAssertion() {
         assertTimeout(Duration.ofSeconds(2),() -> TimeUnit.SECONDS.sleep(10), "Time test fail. Too long executing.");
     }
 
-
     @Test
-    @DisplayName("TimeOut test")
+    @DisplayName("TimeOut test, abort preventive.")
     void testTimeOutAssertionPreventive() {
         assertTimeoutPreemptively(Duration.ofSeconds(5),() -> TimeUnit.SECONDS.sleep(10), "Time test fail. Too long executing.");
     }
 
-
     @Test
-    @DisplayName("Tests all assertions")
+    @DisplayName("Tests all assertions.")
     void testsAllAssertion() {
         assertAll(
                 () -> assertNotNull(null,"Fail, object is null"),
-                () -> assertNotNull(null,"Fail, object is null"),
+                () -> assertNotNull(new Object(),"Fail, object is null"),
                 () -> assertNotNull(null,"Fail, object is null")
         );
+    }
+
+    @Test
+    @Disabled
+    @DisplayName("This test is disabled for many reason.")
+    void hiddenTest(){
+        assertNotEquals(1,4);
     }
 
 }
